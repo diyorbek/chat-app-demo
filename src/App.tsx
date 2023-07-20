0c2201d2039d0b90bdc710f81e0bafb375589615
Diyorbek <diyorbek3377@gmail.com>
2023-07-20 12:58:52 +0500

    feat: Connect controllers to components

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import { ChatMessage } from './components/ChatMessage';
 import { ChatContainer } from './components/ChatContainer';
 import { ChatList } from './components/ChatList';
 import { ChatListItem } from './components/ChatListItem';
+import { fileController } from './FileController';
+import { useChatControllerContext } from './contexts/ChatControllerContext';
+import { chatController } from './ChatController';
+
+const userId = 12345;
 
 function App() {
+  const { archivedChats, currentChat, setCurrentChat } =
+    useChatControllerContext();
+
   return (
     <Container>
       <Box display="flex" flexDirection="column" height="100vh">
@@ -16,37 +24,64 @@ function App() {
         >
           <Toolbar>
             <Box display="flex" gap={2}>
-              <Button variant="outlined" color="info">
+              <Button
+                variant="outlined"
+                color="info"
+                onClick={() => {
+                  void fileController.loadArchiveFromFile().then((content) => {
+                    if (content) chatController.loadArchivedChats(content);
+                  });
+                }}
+              >
                 Import
               </Button>
-              <Button variant="outlined" color="info">
+              <Button
+                variant="outlined"
+                color="info"
+                disabled={!archivedChats.length}
+                onClick={() => {
+                  void fileController.saveArchiveToFile(archivedChats);
+                }}
+              >
                 Export
               </Button>
-              <Button variant="outlined" color="info">
+              <Button
+                variant="outlined"
+                color="info"
+                disabled={!archivedChats.length}
+                onClick={() => chatController.flushArchives()}
+              >
                 Clear
               </Button>
             </Box>
           </Toolbar>
         </AppBar>
 
-        <Box display="flex" height="100%">
-          <Box width="360px">
+        <Box display="flex" overflow="auto" height="100%">
+          <Box width="360px" overflow="auto">
             <ChatList>
-              <ChatListItem label="John Doe" />
-              <ChatListItem label="John Doe" isHighlighted />
-              <ChatListItem label="John Doe" isActive />
-              <ChatListItem label="John Doe" isLast />
+              {archivedChats.map((chat) => (
+                <ChatListItem
+                  key={chat.user.id}
+                  label={chat.user.name}
+                  onClick={() => setCurrentChat(chat)}
+                />
+              ))}
             </ChatList>
           </Box>
 
           <Box flex={1} bgcolor="grey">
-            <ChatContainer>
-              <ChatMessage content="Hello! sdfds" timestamp="" variant="sent" />
-              <ChatMessage
-                content="Hello! sdfds"
-                timestamp="2023-07-18T06:56:43.220Z"
-                variant="received"
-              />
+            <ChatContainer key={currentChat?.user.id || '1'}>
+              {currentChat?.messages.map(
+                ({ content, timestamp, user_id }, i) => (
+                  <ChatMessage
+                    key={i}
+                    content={content}
+                    timestamp={timestamp}
+                    variant={user_id === userId ? 'sent' : 'received'}
+                  />
+                )
+              )}
             </ChatContainer>
           </Box>
         </Box>
