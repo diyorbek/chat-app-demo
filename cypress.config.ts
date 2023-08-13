0062eea9139971e7afe366c3525a5d644460bc15
Diyorbek <diyorbek3377@gmail.com>
2023-08-13 07:43:58 +0200

    fix: Build job

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-
     baseUrl: 'http://localhost:5173/',
     defaultCommandTimeout: 5000,
     video: true,
